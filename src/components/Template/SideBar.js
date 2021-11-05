8929fa360ead97c97258f8683dc9039a715e8b97
Otakar Hirš <otakar.hirs@gmail.com>
2021-11-05 21:50:53 +0100

    sidebar

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,32 +12,32 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+        <h2>Otakar Hirš</h2>
+        <p><a href="mailto:otakar.hirs@gmail.com">otakar.hirs@gmail.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-        I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-        the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
-        at <a href="https://matroid.com">Matroid</a>
-        , <a href="https://planet.com">Planet</a>
-        , <a href="https://planetaryresources.com">Planetary Resources</a>
-        , <a href="https://facebook.com">Facebook</a>
-        , and <a href="https://seds.org">SEDS</a>.
+      <p>Hi, I&apos;m Otakar.
+        I am a friendly guy
+        of <a href="https://www.16personalities.com/intp-personality">INTP-A</a> personality,
+        studying at Faculty of Informatics to b̶e̶c̶o̶m̶e̶ ̶f̶i̶l̶t̶h̶y̶ ̶r̶i̶c̶h̶
+        explore the everchanging nature of IT world.
       </p>
       <ul className="actions">
         <li>
-          {!window.location.pathname.includes('/resume') ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>}
+          {<Link to="/about" className="button">Read more</Link>}
+          {/* {!window.location.pathname.includes('/resume')
+          ? <Link to="/resume" className="button">Learn More</Link>
+          : <Link to="/about" className="button">About Me</Link>} */}
         </li>
       </ul>
     </section>
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Otakar Hirš <Link to="/">otahirs.cz</Link>.</p>
     </section>
   </section>
 );
