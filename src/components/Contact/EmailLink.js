604976f8d5687abd8f9ebde0819a79a397e88c5e
Otakar Hirš <otakar.hirs@gmail.com>
2021-11-05 21:50:36 +0100

    contact

--- src/components/Contact/EmailLink.js
+++ src/components/Contact/EmailLink.js
@@ -76,9 +76,9 @@ const EmailLink = () => {
       onMouseEnter={() => setIsActive(false)}
       onMouseLeave={() => (idx < messages.length) && setIsActive(true)}
     >
-      <a href={validateText(message) ? `mailto:${message}@mldangelo.com` : ''}>
+      <a href={validateText(message) ? `mailto:${message}@otahirs.cz` : ''}>
         <span>{message}</span>
-        <span>@mldangelo.com</span>
+        <span>@otahirs.cz</span>
       </a>
     </div>
   );
