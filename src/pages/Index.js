0ccfaf80bdfd170c3668fb65986f79bdf7502c31
Otakar Hirš <otakar.hirs@gmail.com>
2021-11-05 21:56:43 +0100

    index

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,16 +5,14 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
-    + 'co-founder and CTO of Arthena, and YC Alumni.'}
+    description={"Otakar Hirš's personal website. IT student, orienteer, evening philosopher."}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
           <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
           <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
+            Don&apos;t read boring introductions and start clicking around.
           </p>
         </div>
       </header>
@@ -22,10 +20,11 @@ const Index = () => (
         or you can check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
         <Link to="/projects">projects</Link>, {' '}
-        view <Link to="/stats">site statistics</Link>, {' '}
-        or <Link to="/contact">contact</Link> me.
+        {/* view <Link to="/stats">site statistics</Link>, {' '} */}
+        and soon much more.
+        {/* <Link to="/contact">contact</Link> me. */}
       </p>
-      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
+      <p> Source available <a href="https://github.com/otahirs/personal-site">here</a>.</p>
     </article>
   </Main>
 );
