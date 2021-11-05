8f1c2e87a8280efc337c92ba03b2001ed27d966d
Otakar Hirš <otakar.hirs@gmail.com>
2021-11-05 21:48:12 +0100

    projects

--- src/components/Projects/Cell.js
+++ src/components/Projects/Cell.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import dayjs from 'dayjs';
+// import dayjs from 'dayjs';
 
 const Cell = ({ data }) => (
   <div className="cell-container">
     <article className="mini-post">
       <header>
         <h3><a href={data.link}>{data.title}</a></h3>
-        <time className="published">{dayjs(data.date).format('MMMM, YYYY')}</time>
+        <p className="published">{data.subtitle}</p>
       </header>
       <a href={data.link} className="image">
         <img src={`${process.env.PUBLIC_URL}${data.image}`} alt={data.title} />
@@ -22,9 +22,10 @@ const Cell = ({ data }) => (
 Cell.propTypes = {
   data: PropTypes.shape({
     title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
     link: PropTypes.string,
     image: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
+    // date: PropTypes.string.isRequired,
     desc: PropTypes.string.isRequired,
   }).isRequired,
 };
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,47 +1,34 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
+    title: 'ClubIS',
+    subtitle: 'Currently under development.',
+    link: 'https://github.com/otahirs/ClubIS',
+    image: '/images/projects/clubis.png',
     desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+      'Information system for an orienteering club, including management of members data, event entries and finance, '
+      + 'with focus on easy of use, automation and integration with other systems.',
   },
   {
-    title: 'Harvest',
-    subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
-    link: 'https://devpost.com/software/harvest',
-    image: '/images/projects/harvest.jpg',
-    date: '2015-09-20',
+    title: 'QuickEvent',
+    subtitle: 'Open source projects <3',
+    link: 'https://github.com/Quick-Event/quickbox',
+    image: '/images/projects/quickevent.png',
     desc:
-      'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
-      + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
-      + 'and nutrient deficiencies at an affordable price for the developing world.',
+      'Not a project founder, but finnaly a regular contributor. Feature rich open source '
+      + 'software for organizing orienteering evets. '
+      + 'Written in C++, built upon the Qt framework, '
+      + 'targeted for both Linux and Windows and released under the GPL-2.0 License.',
   },
   {
-    title: 'Space Potato',
-    subtitle: 'A kickstarter funded potato powered weather balloon.',
-    link: 'http://www.spacepotato.org',
-    image: '/images/projects/spacepotato.jpg',
-    date: '2015-06-28',
+    title: 'SK brno žabovřesky',
+    subtitle: 'From nothing to usefull software',
+    link: 'https://github.com/otahirs/zbm_web',
+    image: '/images/projects/zabiny.png',
     desc:
-      'Launched a potato battery powered weather balloon with two cameras '
-      + 'and gps transponder. Resulting photos were published in a coffee table book. '
-      + 'You can email me for a copy.',
-  },
-  {
-    title: 'Cat Detector',
-    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
-    image: '/images/projects/catdetector.jpg',
-    date: '2015-05-15',
-    desc:
-      'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-      + 'Over 60,000 cats were classified before server bills made the project too expensive '
-      + 'to continue hosting.',
+      'From no knowledge that JavaScript is not Java into fully functioning '
+      + 'website with dynamic content generation and custom content managment system. '
+      + 'Proof that with enought time and endless refactoring, anything is posssible.',
   },
 ];
 
